49d9b8e9b722d163ec247e416cc5eec9c9f492d4
Jeremy Mates <jmates@cpan.org>
2019-01-22 07:47:04 -0800

    also original search code for comparison

--- util/door-search.c
+++ util/door-search.c
@@ -6,23 +6,35 @@
 
 #include "rogue.h"
 
-#define SEEDS 1024
+#define SEEDS  1024
 #define TRIALS 1024
 
 int search(void);
+int new_search(void);
 
 int main(int argc, char *argv[])
 {
     for (int s = 0; s < SEEDS; s++) {
         seed = (int) arc4random();
         for (int t = 0; t < TRIALS; t++) {
-            printf("%d\n", search());
+            printf("%d\n", new_search());
         }
     }
     exit(0);
 }
 
 int search(void)
+{
+    int turns = 1;
+    while (1) {
+        if (rnd(100) < 20)
+            break;
+        turns++;
+    }
+    return turns;
+}
+
+int new_search(void)
 {
     int turns = 0;
     while (1) {
